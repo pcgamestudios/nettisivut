c89f9a0b17765a76d8014fb22042ee1848b0756b
pcgamestudios <77838577+pcgamestudios@users.noreply.github.com>
2021-01-31 16:28:02 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ paypal.Buttons({
         return actions.order.create({
             purchase_units : [{
                 amount: {
-                    value: '0.1'
+                    value: '24'
                 }
             }]
         });
